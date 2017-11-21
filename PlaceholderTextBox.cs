182c1cb881784ffb68059a55c5739cc02f375122
ase69s <ase69s@gmx.com>
2017-11-21 17:56:46 +0100

    Replace Text.Substringx to Text.Substring
    Replace Text.Substringx to Text.Substring

--- PlaceholderTextBox.cs
+++ PlaceholderTextBox.cs
@@ -222,7 +222,7 @@ namespace System.Windows.Forms
                           if (Modified)//Only do this part if the event was fired from user input, in case of setting text programmatically, all the text is replaced
                           {
                               // Throw away the placeholder but leave the new typed text
-                              Text = Text.Substringx(0, TextLength - PlaceholderText.Length);
+                              Text = Text.Substring(0, TextLength - PlaceholderText.Length);
 
                               // Set Selection to last position
                               Select(TextLength, 0);
